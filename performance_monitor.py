f29a18ae2d1301c91419afaf0919e78add41433c
AlexanderMomtaz <alexander@magenta.dk>
2017-08-31 11:48:47 +0200

    Added a touch url for measuring the responsetime from connecting directly to the loadbalancer.

--- performance_monitor.py
+++ performance_monitor.py
@@ -5,7 +5,7 @@ import json
 import csv
 
 
-def get_token(config, req):
+def get_tokenheader(config, req):
     if req is None:
         username = config['username']
         password = config['password']
@@ -13,10 +13,28 @@ def get_token(config, req):
         username = req['username']
         password = req['password']
     tokenurl = config['tokenurl'] + "?username=" + username + "&password=" + password
-    return requests.get(tokenurl).text
+    token = requests.get(tokenurl).text
+    return {'Authorization': 'SAML ' + token }
 
 def get_avg_time(times):
-    return format(sum(times) / len(times))
+    return sum(times) / len(times)
+
+def get_response_time(url, headers, repetitions):
+    responsetimes = []
+    responsetimeswithcontent = []
+    r = requests.get(url, headers=headers)
+
+    for i in repetitions:
+        print 'Requesting iteration #' + str(i)
+        start = time.time()
+        r = requests.get(url, headers=headers)
+        end = time.time() - start
+        responsetimes.append(r.elapsed.total_seconds())
+        responsetimeswithcontent.append(end)
+
+    responsetimeavg = get_avg_time(responsetimes)
+    responsetimewithcontentavg = get_avg_time(responsetimeswithcontent)
+    return responsetimeavg, responsetimewithcontentavg
 
 #Open output file
 with open('output.csv', 'wb') as csvfile:
@@ -27,30 +45,23 @@ with open('output.csv', 'wb') as csvfile:
     #Read requests from settings
     with open('settings.json', 'r') as f:
         config = json.load(f)
-        stdtoken = get_token(config, None)
+        repetitions = range(config['repetitions'])
+        stdheaders = get_tokenheader(config, None)
+
+        #Get response time for load balancer
+        url = config['touchurl']
+        lb_timeavg, lb_timeavg_cont = get_response_time(url, stdheaders, repetitions)
+        csvwriter.writerow(["to loadbalancer", lb_timeavg, lb_timeavg_cont])
 
         #Get response time for each request
         for req in config['requests']:
-            responsetimes = []
-            responsetimeswithcontent = []
-
             url = config['domainurl'] + req['url']
-            print 'Requesting ' + req['url']
             if req.get('username') is None:
-                headers = {'Authorization': 'SAML ' + stdtoken }
+                headers = stdheaders
             else:
-                token = get_token(config, req)
-                headers = {'Authorization': 'SAML ' + token }
-
-            r = requests.get(url, headers=headers)
-            for i in range(config['repetitions']):
-                start = time.time()
-                r = requests.get(url, headers=headers)
-                end = time.time() - start
-                responsetimes.append(r.elapsed.total_seconds())
-                responsetimeswithcontent.append(end)
-
-            responsetimeavg = get_avg_time(responsetimes)
-            responsetimeswithcontentavg = get_avg_time(responsetimeswithcontent)
-
-            csvwriter.writerow([req['url'], responsetimeavg, responsetimeswithcontentavg])
+                headers = get_tokenheader(config, req)
+            print 'Requesting ' + url
+            timeavg, timeavg_cont = get_response_time(url, headers, repetitions)
+            timeavg = format(timeavg - lb_timeavg)
+            timeavg_cont = format(timeavg_cont - lb_timeavg_cont)
+            csvwriter.writerow([url, timeavg, timeavg_cont])
